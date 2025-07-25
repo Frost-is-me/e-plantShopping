e7729fc867a3cdbf9911232affb25282938ca02e
Frost <murtada.najm99@gmail.com>
2025-07-25 06:11:29 +0300

    final update.2

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://github.com/Frost-is-me/e-plantShopping.git",
+  base: "https://github.com/Frost-is-me/e-plantShopping",
   plugins: [react()],
 })
